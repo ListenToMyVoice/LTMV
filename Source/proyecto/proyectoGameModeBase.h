eb0f4c26c91d0ba16963262d5f9b4c1cc350ea12
Alex <afcarrion85@gmail.com>
2016-12-09 13:01:33 +0100

    Project created empty in c++

---
+++ Source/proyecto/proyectoGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "proyectoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROYECTO_API AproyectoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
