7041ff9633a992ba3b17dde9cdbcdfb5ad882ce1
Alex <afcarrion85@gmail.com>
2017-01-13 11:08:42 +0100

    gamemodebase to gamemode

--- Source/proyecto/proyectoGameMode.cpp
+++ Source/proyecto/proyectoGameMode.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "proyecto.h"
-#include "proyectoGameModeBase.h"
+#include "proyectoGameMode.h"
 
-AproyectoGameModeBase::AproyectoGameModeBase(const class FObjectInitializer& OI) : Super(OI) {
+AproyectoGameMode::AproyectoGameMode(const class FObjectInitializer& OI) : Super(OI) {
     static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT(
         "/Game/Meshes/Skeletons/Prototypes/Player/PlayerCharacter_BP"));
     DefaultPawnClass = PlayerPawnClassFinder.Class;
---
+++ Source/proyecto/proyectoGameMode.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "proyectoGameMode.generated.h"
+
+UCLASS()
+class PROYECTO_API AproyectoGameMode : public AGameMode {
+    GENERATED_BODY()
+
+public:
+    AproyectoGameMode(const class FObjectInitializer& OI);
+};
