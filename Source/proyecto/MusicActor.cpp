b18cfd3823c547534fdf77d963c90eceb96ca1dd
Alex <afcarrion85@gmail.com>
2017-01-24 13:55:23 +0100

    music actor added

---
+++ Source/proyecto/MusicActor.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "proyecto.h"
+#include "MusicActor.h"
+
+
+AMusicActor::AMusicActor() {
+    _mainTrackPath = TEXT("/Game/Audio/Music/Midnight_Syndicate-3-Born_of_the_Night");
+    _actionTrackPath = TEXT("/Game/Audio/Music/04_-_Black_Night_Of_Magic");
+
+    SetMusic(EMusicType::Main);
+}
+
+void AMusicActor::BeginPlay() {
+    Super::BeginPlay();
+}
+
+void AMusicActor::SetMusic(EMusicType type) {
+    FString toPlay = _mainTrackPath;
+    if (type == EMusicType::Main) {
+        _playing = EMusicType::Main;
+    }
+    else if (type == EMusicType::Action) {
+        _playing = EMusicType::Action;
+    }
+
+    static ConstructorHelpers::FObjectFinder<USoundWave> Sound(*toPlay);
+    GetAudioComponent()->SetSound(Sound.Object);
+}
