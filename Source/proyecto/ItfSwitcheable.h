113ca58b2f0599a23561a917d41a77a554c8868c
Alex <afcarrion85@gmail.com>
2017-02-01 13:52:12 +0100

    new FSwitcher

--- Source/proyecto/ItfSwitcheable.h
+++ Source/proyecto/ItfSwitcheable.h
@@ -2,21 +2,6 @@
 
 #include "ItfSwitcheable.generated.h"
 
-USTRUCT(BlueprintType, Category = "ToggleActor")
-struct FSwitcheable {
-    GENERATED_USTRUCT_BODY()
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ToggleActor")
-    AActor* _actor;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ToggleActor")
-    TArray<FName> _components;
-
-    FSwitcheable() {
-        _actor = nullptr;
-        _components = {};
-    }
-};
 
 UINTERFACE(Blueprintable)
 class PROYECTO_API UItfSwitcheable : public UInterface {
@@ -28,6 +13,62 @@ class PROYECTO_API IItfSwitcheable {
 
 public:
 
-    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Item")
+    UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Switch")
     int SwitchState();
+};
+
+
+USTRUCT(BlueprintType, Category = "Switch")
+struct FSwitcheable {
+    GENERATED_USTRUCT_BODY()
+
+        UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Switch")
+        AActor* _actor;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Switch")
+        TArray<FName> _components;
+
+    FSwitcheable() {
+        _actor = nullptr;
+        _components = {};
+    }
+};
+
+USTRUCT(BlueprintType, Category = "Switch")
+struct FSwitcher {
+    GENERATED_USTRUCT_BODY()
+
+private:
+    TArray<UActorComponent*> SwitcheableComps;
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Switch")
+    TArray<FSwitcheable> SwitcheableArr;
+
+    FSwitcher() {
+        SwitcheableArr = {};
+        SwitcheableComps = {};
+    }
+
+    void InitSwitcher() {
+        /* Fill the references to other switcheable componets */
+        TArray<UActorComponent*> components = {};
+        for (FSwitcheable s : SwitcheableArr) {
+            s._actor->GetComponents(components);
+            for (UActorComponent* comp : components) {
+                if (s._components.IndexOfByKey(comp->GetFName()) != INDEX_NONE) {
+                    SwitcheableComps.AddUnique(comp);
+                }
+            }
+        }
+    }
+
+    void ActivateSwitcher() {
+        for (UActorComponent* component : SwitcheableComps) {
+            if (component->GetClass()->ImplementsInterface(UItfSwitcheable::StaticClass())) {
+                IItfSwitcheable* itfObject = Cast<IItfSwitcheable>(component);
+                if (itfObject) itfObject->Execute_SwitchState(component);
+            }
+        }
+    }
 };
