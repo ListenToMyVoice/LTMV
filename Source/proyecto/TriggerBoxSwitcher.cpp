7d0b91f9255026f9990d5e43cbe02356782a533d
Alex <afcarrion85@gmail.com>
2017-02-03 01:32:40 +0100

    map

--- Source/proyecto/TriggerBoxSwitcher.cpp
+++ Source/proyecto/TriggerBoxSwitcher.cpp
@@ -19,7 +19,6 @@ void ATriggerBoxSwitcher::BeginPlay() {
 void ATriggerBoxSwitcher::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
                                          UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
                                          bool bFromSweep, const FHitResult& SweepResult) {
-    ULibraryUtils::Log(TEXT("caca"));
     _switcher.ActivateSwitcher();
     if (--_numTimesActive == 0) SetActorEnableCollision(false);
 }
