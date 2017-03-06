e2fb5944a027768d1371f9eb2994a17747f1e211
katnisx <andrea.aos.carrasco@gmail.com>
2017-03-06 22:12:36 +0100

    New Actor Component - LightPressState, new lights in map

---
+++ Source/proyecto/LightPressState.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "proyecto.h"
+#include "LightPressState.h"
+
+
+// Sets default values for this component's properties
+ULightPressState::ULightPressState()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	_on = false;//La luz inicia como apagada
+}
+
+
+// Called when the game starts
+void ULightPressState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Iniciar la luz en apagado
+	_lightComp = Cast<ULightComponent>(GetOwner()->GetComponentByClass(ULightComponent::StaticClass()));
+	ULibraryUtils::TestNull(_lightComp);
+
+}
+
+
+// Called every frame
+void ULightPressState::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	if (_on) {
+		_lightComp->SetIntensity(500.0f);
+
+	}
+	else {
+		_lightComp->SetIntensity(0.0f);
+	}
+}
+
+
+int ULightPressState::SwitchState2_Implementation() {
+	//Encender la luz durante el press
+	_on = true;
+	_current_state = _on;
+	return 0;
+}
+int ULightPressState::SwitchState_Implementation() {
+	//luz apagada en el release
+	_on = false;
+	_current_state = _on;
+	return 0;
+}
