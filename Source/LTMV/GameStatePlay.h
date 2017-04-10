a33a8688a6e5854f6d6ec2f21d458af206cee693
Alex <afcarrion85@gmail.com>
2017-04-10 10:05:58 +0200

    classes refactor

---
+++ Source/LTMV/GameStatePlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "GameStatePlay.generated.h"
+
+
+UCLASS()
+class LTMV_API AGameStatePlay : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
