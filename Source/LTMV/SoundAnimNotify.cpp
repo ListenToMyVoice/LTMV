02bdd1164b4c094d697ebaebc1f6b5140c03e055
Alex <afcarrion85@gmail.com>
2017-05-23 12:14:15 +0200

    fix sound notify

--- Source/LTMV/SoundAnimNotify.cpp
+++ Source/LTMV/SoundAnimNotify.cpp
@@ -5,14 +5,18 @@
 #include "FMODAudioComponent.h"
 
 
-USoundAnimNotify::USoundAnimNotify(const FObjectInitializer& OI) : Super(OI) {}
+USoundAnimNotify::USoundAnimNotify(const FObjectInitializer& OI) : Super(OI) {
+    _Tag = "";
+}
 
 void USoundAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) {
-    TArray<UActorComponent*> Components = MeshComp->GetOwner()->GetComponentsByTag(UFMODAudioComponent::StaticClass(), _Tag);
-    for (UActorComponent* Component : Components) {
-        UFMODAudioComponent* FMODComp = Cast<UFMODAudioComponent>(Component);
-        if (FMODComp) {
-            FMODComp->Play();
+    if (MeshComp->GetOwner()) {
+        TArray<UActorComponent*> Components = MeshComp->GetOwner()->GetComponentsByTag(UFMODAudioComponent::StaticClass(), _Tag);
+        for (UActorComponent* Component : Components) {
+            UFMODAudioComponent* FMODComp = Cast<UFMODAudioComponent>(Component);
+            if (FMODComp) {
+                FMODComp->Play();
+            }
         }
     }
 }
